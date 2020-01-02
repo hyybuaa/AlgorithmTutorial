a31cd459326b6da36d871df252ef5599818cffcd
oleg <oleg@olegdeMacBook-Pro.local>
2020-01-03 01:09:00 +0800

    完成09 11题

--- Fibonacci.py
+++ Fibonacci.py
@@ -3,7 +3,19 @@ class Solution:
     def Fibonacci(self, n):
         # write code here
         # 利用动态规划算法
-        pass
+        if n<=0:
+            return 0
+        array = [0]*n
+        if n==1 or n==2:
+            return 1
+        array[0] = 1
+        array[1] = 1
+        for i in range(3, n+1):
+            array[i-1] = array[i-3] + array[i-2]
+        return array[n-1]
+
+
+
 
 if __name__ == "__main__":
     n = 5
